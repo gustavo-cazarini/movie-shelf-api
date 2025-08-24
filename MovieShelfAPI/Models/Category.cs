d5fc8dbe03e2fa40d3f4ee05d81e046dfbf2bc3a
Gustavo Cazarini <gustavo.cazarini@outlook.com>
2025-08-24 19:32:58 -0300

    refat: added necessary property to auto-identify n-to-n relashin between the models

--- MovieShelfAPI/Models/Category.cs
+++ MovieShelfAPI/Models/Category.cs
@@ -7,7 +7,10 @@ namespace MovieShelfAPI.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MinLength(1)]
+        [MaxLength(100)]
         public string Name { get; set; }
         public List<Movie> Movies { get; } = [];
+        public List<MovieCategory> MovieCategories { get; } = [];
     }
 }
--- MovieShelfAPI/Models/Movie.cs
+++ MovieShelfAPI/Models/Movie.cs
@@ -7,11 +7,14 @@ namespace MovieShelfAPI.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MinLength(1)]
+        [MaxLength(100)]
         public string Name { get; set; }
         public DateTime ReleaseDate { get; set; }
-        public string Director { get; set; }
-        public string Cast { get; set; }
-        public string Synopsis { get; set; }
+        public string? Director { get; set; }
+        public string? Cast { get; set; }
+        public string? Synopsis { get; set; }
         public List<Category> Categories { get; } = [];
+        public List<MovieCategory> MovieCategories { get; } = [];
     }
 }
